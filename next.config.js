4bb3977b3e5b0e40aa1133a79abf53acb690252a
luismartinez375 <luisleonardomartinez375@live.com>
2023-07-17 15:43:09 -0500

    feat: added images and final touches

--- next.config.js
+++ next.config.js
@@ -1,17 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['preview.redd.it'],
-        formats: ['image/avif', 'image/webp'],
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'preview.redd.it',
-            port: '',
-          },
-        ],
+  source: '/(.*)',
+  headers: [
+    {
+      key: 'Referrer-Policy',
+      value: 'origin-when-cross-origin',
+    },
+  ],
+  images: {
+    domains: [
+      'external-preview.redd.it',
+      'preview.redd.it',
+      'a.thumbs.redditmedia.com',
+      'thumbs.redditmedia.com',
+      'b.thumbs.redditmedia.com',
+    ],
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**external-preview.redd.it',
+        hostname: '**thumbs.redditmedia.com',
+        port: '',
       },
-    }
-
+    ],
+  },
+};
 
 module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,36 @@
-import { Post, Posts } from "../../types";
-import getData from "./api/getPosts";
-import CardItem from "./components/cardItem/cardIem";
+import getData, { PostClass } from './api/getPosts';
+import CardItem from './components/cardItem/cardIem';
 export default async function Home() {
-   
-  let postlist: Posts | undefined= await getData()
-  let posts = JSON.parse(JSON.stringify(postlist))
-  console.log(posts)
-  
-  return (
-    <main className="flex flex-col items-center min-h-screen bg-gradient-to-l from-orange-600 to-orange-400 ">
-      <div className="relative right-1/4 p-5">
-      <h1 className="text-l font-roboto text-white">Posts</h1>
-      <div className=" bg-postsDiv h-0.5 w-14 relative -left-2 ">
+  let postlist: PostClass[] | undefined = await getData();
+  let posts = JSON.parse(JSON.stringify(postlist));
+  // console.log(posts);
 
-      </div>
+  return (
+    <main className="flex flex-col items-center min-h-screen bg-gradient-to-l from-orange-700 to-orange-400 ">
+      <div className="relative right-1/4 p-5">
+        <h1 className="text-l font-roboto text-white">Posts</h1>
+        <div className=" bg-postsDiv h-0.5 w-14 relative -left-2 "></div>
       </div>
       <ul className="flex flex-col items-center gap-5">
-    {posts.map((post: Post) => 
-
-      {
-        const keys = post.media_metadata ?   Object.keys(post.media_metadata): undefined;
-        keys ? console.log(post.media_metadata[keys[0]].s.u) : console.log("no keys")
-
-      return(
-      
-      <CardItem key={post.url} author={post.author} media_metadata={undefined} title={""} thumbnail={""} url={""} data={{
-        title: post.title,
-        author: post.author,
-        thumbnail: '',
-        url: post.url,
-        media_metadata: keys ?  post.media_metadata[keys[0]].s.u : undefined
-      }}  ></CardItem>
-    )})}
-    </ul>
+        {posts.map((post: PostClass) => {
+          const keys: string[] | undefined = post.mediaMetadata
+            ? Object.keys(post.mediaMetadata)
+            : undefined;
+          keys
+            ? console.log(post.mediaMetadata[keys[0]].s.u)
+            : console.log('no keys');
+          return (
+            <CardItem
+              key={post.url}
+              author={post.author}
+              mediaMetadata={keys ? post.mediaMetadata[keys[0]].s.u : undefined}
+              title={post.title}
+              thumbnail={post.thumbnail}
+              url={post.url}
+            ></CardItem>
+          );
+        })}
+      </ul>
     </main>
   );
 }
